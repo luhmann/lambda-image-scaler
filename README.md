92766543f96614c696531575113328f6f24a0731
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2018-03-23 12:05:53 +0100

    feat(cache): implement cache for resized images

--- README.md
+++ README.md
@@ -5,3 +5,28 @@
 ```
 docker run -e AWS_SECRET_ACCESS_KEY='<SECRET_ACCESS_KEY>' -e AWS_ACCESS_KEY_ID='<ACCESS_KEY_ID>' -v /Users/jfd/dev/lambda-image-scaler:/var/task lambci/lambda:build-nodejs6.10 ./node_modules/.bin/claudia update --version dev --use-local-dependencies
 ```
+
+## Bucket Policy to set on the s3 bucket
+
+*   The `principal` must be the role that claudia created for you in IAM
+*   The `resource` must be the arn of the bucket with `/*` in the end to allow access to objects
+
+```json
+{
+    "Id": "Policy1521800101848",
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Stmt1521800097305",
+            "Action": "s3:*",
+            "Effect": "Allow",
+            "Resource": "arn:aws:s3:::image-scaler/*",
+            "Principal": {
+                "AWS": [
+                    "arn:aws:iam::774843948343:role/punk-api-scaler-executor"
+                ]
+            }
+        }
+    ]
+}
+```
--- src/scaler.js
+++ src/scaler.js
@@ -19,65 +19,90 @@ const isAllowedSize = width => ALLOWED_SIZES.has(parseInt(width, 10));
 const getRenamedFilename = (size, filename) =>
   `${RESIZED_PATH}/${size}_${path.basename(filename)}`;
 
+const getResizeResponse = (width, filename) =>
+  new api.ApiResponse(
+    '',
+    {
+      location: `https://s3.eu-central-1.amazonaws.com/${BUCKET}/${getRenamedFilename(
+        width,
+        filename
+      )}`,
+    },
+    301
+  );
+
 api.get('{width}/{imagePath}', request => {
   if (!isAllowedSize(request.pathParams.width)) {
     return new api.ApiResponse('Forbidden', { ContentType: 'text/plain' }, 403);
   }
 
   return s3
-    .getObject({
+    .headObject({
       Bucket: BUCKET,
-      Key: `${SRC_PATH}/${request.pathParams.imagePath}`,
+      Key: getRenamedFilename(
+        request.pathParams.width,
+        request.pathParams.imagePath
+      ),
     })
     .promise()
     .then(data => {
-      console.log('received source image');
-      return sharp(data.Body)
-        .resize(parseInt(request.pathParams.width))
-        .background({ r: 255, g: 255, b: 255, alpha: 1 })
-        .flatten()
-        .jpeg({ quality: 60, progressive: true })
-        .toBuffer();
-    })
-    .then(buffer => {
-      console.log('transformed image');
-      return s3
-        .putObject({
-          Body: buffer,
-          Bucket: BUCKET,
-          ContentType: 'image/jpeg',
-          Key: `${getRenamedFilename(
-            request.pathParams.width,
-            request.pathParams.imagePath
-          )}`,
-          ACL: 'public-read',
-        })
-        .promise()
-        .catch(err => {
-          console.log('error in saving resized image', err);
-          throw err;
-        });
-    })
-    .then(() => {
-      return new api.ApiResponse(
-        '',
-        {
-          location: `https://s3.eu-central-1.amazonaws.com/${BUCKET}/${getRenamedFilename(
-            request.pathParams.width,
-            request.pathParams.imagePath
-          )}`,
-        },
-        301
+      console.log('Found resized object, last modified at', data.LastModified);
+      return getResizeResponse(
+        request.pathParams.width,
+        request.pathParams.imagePath
       );
     })
     .catch(err => {
-      console.log('Error', err);
+      console.log('No resized image found', err);
+      return s3
+        .getObject({
+          Bucket: BUCKET,
+          Key: `${SRC_PATH}/${request.pathParams.imagePath}`,
+        })
+        .promise()
+        .then(data => {
+          console.log('received source image');
+          return sharp(data.Body)
+            .resize(parseInt(request.pathParams.width))
+            .background({ r: 255, g: 255, b: 255, alpha: 1 })
+            .flatten()
+            .jpeg({ quality: 60, progressive: true })
+            .toBuffer();
+        })
+        .then(buffer => {
+          console.log('transformed image');
+          return s3
+            .putObject({
+              Body: buffer,
+              Bucket: BUCKET,
+              ContentType: 'image/jpeg',
+              Key: `${getRenamedFilename(
+                request.pathParams.width,
+                request.pathParams.imagePath
+              )}`,
+              ACL: 'public-read',
+            })
+            .promise()
+            .catch(err => {
+              console.log('error in saving resized image', err);
+              throw err;
+            });
+        })
+        .then(() =>
+          getResizeResponse(
+            request.pathParams.width,
+            request.pathParams.imagePath
+          )
+        )
+        .catch(err => {
+          console.log('Error', err);
 
-      return new api.ApiResponse(
-        'Service Unavailable',
-        { 'Content-Type': 'text/plain' },
-        503
-      );
+          return new api.ApiResponse(
+            'Service Unavailable',
+            { 'Content-Type': 'text/plain' },
+            503
+          );
+        });
     });
 });
 
